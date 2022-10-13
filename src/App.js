7d809bf8b24b4fa781daeef0a43022cc972fdc23
aliciams <# apt-get install git>
2022-10-13 10:56:22 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import dayjs from "dayjs";
+
+const isSameOrAfter = require("dayjs/plugin/isSameOrAfter");
+const isSameOrBefore = require("dayjs/plugin/isSameOrBefore");
+dayjs.extend(isSameOrBefore);
+dayjs.extend(isSameOrAfter);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [allData, setData] = useState(data);
+
+const handleFilterDate = (date, field) => {
+  const filteredData = data.filter((item) => {
+    if (field === "from" && dayjs(item.date).isSameOrAfter(dayjs(date))) {
+      return item;
+    }
+  });
+
+  setData(filteredData);
+};
+
+return (
+  <div className="container">
+    <div className="row">
+      <div className="col-sm-3">
+        <FilterBar
+          onDateFilter={handleFilterDate}
+        />
+      </div>
+      <div className="col-sm-9">
+        <div className="row mt-5">
+          {allData.map((item) => (
+            <PersonItem item={item} key={item.id} />
+          ))}
+        </div>
+      </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
